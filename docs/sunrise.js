49504f3ab19de2966735847980c5cdb65b2c6338
simplizm-company <40747170+simplizm-company@users.noreply.github.com>
2019-04-01 09:47:31 +0900

    body fixed

--- docs/sunrise.js
+++ docs/sunrise.js
@@ -56,7 +56,8 @@
     Sunrise.prototype.layoutMarkup = function () {
         var _ = this;
 
-        _.element.body.data('originStyle', _.element.body.attr('style') === undefined ? null : _.element.body.attr('style')).css({'overflow': 'hidden'});
+        _.initial.scrollTop = $(window).scrollTop();
+        _.element.body.addClass('sunrise-fixed').css({'top': -_.initial.scrollTop});
         _.element.outer = _.element.body.append(_.markups.outer).children('.sunrise-outer');
         _.element.inner = _.element.outer.append(_.markups.inner).children('.sunrise-inner');
         _.element.scene = _.element.inner.append(_.markups.scene).children('.sunrise-scene');
@@ -93,7 +94,8 @@
         }
         _.options.closeCallback(_.element.target);
         _.element.outer.remove();
-        _.element.body.attr('style', _.element.body.data('originStyle'));
+        _.element.body.removeClass('sunrise-fixed').css({'top': 0});
+        $(window).scrollTop(_.initial.scrollTop);
     }
 
     Sunrise.prototype.eventsRegister = (function () {
